2b48e3ea9a394551231e49712263aa2a298d3a9a
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-04-18 14:51:27 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 run()
+const allowedOrigins = ['http://localhost:3001', 'https://example.netlify.app'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 app.use(express.json())
-app.use(cors())
-app.use('/auth',router1)
-app.use('/notes',router2)
+app.use(cors(corsOptions))
 
-app.listen(process.env.port||3001, () => {
-  console.log(`Example app listening on port ${process.env.port||3001}`)
+app.use('/auth', router1)
+app.use('/notes', router2)
+
+app.listen(process.env.port || 3001, () => {
+  console.log(`Example app listening on port ${process.env.port || 3001}`)
 })
