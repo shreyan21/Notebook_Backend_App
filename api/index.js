c435db49ffd8777ea0701ce624b626b9dafcd744
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-07-01 23:19:27 +0530

    Fixed methods

--- api/index.js
+++ api/index.js
@@ -11,10 +11,11 @@ run()
 
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('uploads'))
-app.use(cors())
-
+app.use(cors({
+  origin: 'https://notedrive.netlify.app' // Replace with your frontend URL
+}));
 app.use('/auth', router1)
 app.use('/notes', router2)
 
